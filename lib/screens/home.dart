b406417a4e50eb29415c68b4c08e99c271eb9c4b
Luke Alexander <lukealexander2a@gmail.com>
2024-04-02 00:28:41 +0100

    Basic UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -25,13 +25,13 @@ class _HomeScreenState extends State<HomeScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
+                  const Text(
                     "Notes",
                     style: TextStyle(fontSize: 30, color: Colors.white),
                   ),
                   IconButton(
                     onPressed: () {},
-                    padding: EdgeInsets.all(0),
+                    padding: const EdgeInsets.all(0),
                     icon: Container(
                       width: 40,
                       height: 40,
@@ -39,15 +39,92 @@ class _HomeScreenState extends State<HomeScreen> {
                         color: Colors.grey.shade800.withOpacity(0.8),
                         borderRadius: BorderRadius.circular(10),
                       ),
-                      child: Icon(
+                      child: const Icon(
                         Icons.sort,
                         color: Colors.white,
                       ),
                     ),
                   ),
                 ],
-              )
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                style: const TextStyle(fontSize: 16, color: Colors.white),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 12),
+                  hintText: "Search notes...",
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                  fillColor: Colors.grey.shade800,
+                  filled: true,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: ListView(
+                children: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      title: RichText(
+                        text: const TextSpan(
+                            text: 'Like and Subscribe\n',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              height: 1.5,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'Thank you!!!',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 14,
+                                    height: 1.5),
+                              ),
+                            ]),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          'Edited: 20/12/2023',
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.delete,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )),
             ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.grey.shade800,
+          child: const Icon(
+            Icons.add,
+            size: 38,
           ),
         ),
       ),
